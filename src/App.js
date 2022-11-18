f547511051c8ed3b6314a042061e062eceeacad5
iosvaldo <iosvaldo.app@gmail.com>
2022-11-18 06:37:17 +0000

    basic properties 🚨

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import Product from './Product';
+
 
 function App() {
   return (
     <div className="app">
+      {/* <h1>app about props</h1> */}
+     
+      <Product
+        name="iphone"
+        description="The new iphone 14 Pro"
+        price={1200.00} />
       
+      <Product
+        name="Sony Camera"
+        description="he best mirrorless camera "
+        price={800.00} />
+      
+      <Product
+        name="Macbook pro"
+        description="The M1 chip"
+        price={2500.00} />
     </div>
   );
 }
